7d47964be5035e2b25fc08a3739910b93bb32c79
instance01 <nibbadont@protonmail.com>
2020-05-18 12:01:57 +0200

    #1: Improve format of params logs.

--- alphazero/simulate.py
+++ alphazero/simulate.py
@@ -359,143 +359,143 @@ tensor([[nan, nan, nan,  ..., nan, nan, nan],
 --
 
 Params1:
-[2, 4, 0, 8, 3, 99, 1, 99, 7, 1]
-Minutes: 700.5658185561498
+    [2, 4, 0, 8, 3, 99, 1, 99, 7, 1]
+    Minutes: 700.5658185561498
 
 Params2:
-Crashed, gradient explosion
-Running (restarted)
+    Crashed, gradient explosion
+    Running (restarted)
 
 Params3:
-[7, 8, 7, 8, 3, 3, 6, 1, 1, 0]
-Minutes: 602.0077656269074
+    [7, 8, 7, 8, 3, 3, 6, 1, 1, 0]
+    Minutes: 602.0077656269074
 
 Params4:
-Crashed, gradient explosion
-Running (restarted)
-+multiprocessing
-Crashed again, gradient explosion
+    Crashed, gradient explosion
+    Running (restarted)
+    +multiprocessing
+    Crashed again, gradient explosion
 
 Params5:
-Crashed, gradient explosion
+    Crashed, gradient explosion
 
 Params6:
-[4, 99, 12, 4, 99, 30, 3, 2, 7, 99]
-Minutes: 2090.946610148748
+    [4, 99, 12, 4, 99, 30, 3, 2, 7, 99]
+    Minutes: 2090.946610148748
 
 Params7:
-[4, 13, 8, 1, 1, 4, 0, 33, 4, 19]
-Minutes: 562.2411169131597
+    [4, 13, 8, 1, 1, 4, 0, 33, 4, 19]
+    Minutes: 562.2411169131597
 
 Params8:
-Crashed, gradient explosion
+    Crashed, gradient explosion
 
 Params9:
-[8, 6, 13, 4, 1, 2, 99, 2, 3, 1]
-Minutes: 250.394251592954
+    [8, 6, 13, 4, 1, 2, 99, 2, 3, 1]
+    Minutes: 250.394251592954
 
 Params10:
-[13, 1, 10, 20, 3, 4, 1, 6, 3, 1]
-Minutes: 86.78793809811275
-+multiprocessing
+    [13, 1, 10, 20, 3, 4, 1, 6, 3, 1]
+    Minutes: 86.78793809811275
+    +multiprocessing
 
 Params11: (same as Params9)
-+multiprocessing
-+pb_c base is now 50.
-Crashed, pool leaking fds
-+fixed pool
-[4, 11, 29, 59, 10, 2, 6, 12, 24, 8]
-Minutes: 141.41895825068156
+    +multiprocessing
+    +pb_c base is now 50.
+    Crashed, pool leaking fds
+    +fixed pool
+    [4, 11, 29, 59, 10, 2, 6, 12, 24, 8]
+    Minutes: 141.41895825068156
 
 Params12:
-[37, 4, 11, 4, 13, 21, 6, 3, 6, 3]
-Minutes: 126.497244437535
+    [37, 4, 11, 4, 13, 21, 6, 3, 6, 3]
+    Minutes: 126.497244437535
 
 Params13:
-[16, 99, 1, 11, 87, 1, 16, 5, 3, 1]
-Minutes: 278.8917894800504
+    [16, 99, 1, 11, 87, 1, 16, 5, 3, 1]
+    Minutes: 278.8917894800504
 
 Params14:
-[99, 61, 2, 14, 9, 2, 9, 11, 3, 10]
-Minutes: 248.89004709323248
+    [99, 61, 2, 14, 9, 2, 9, 11, 3, 10]
+    Minutes: 248.89004709323248
 
 Params12:  # Since it's the best one so far. (12_1)
-+goal_pos=[3,-3]
-+borders=[7,-7]
-+max_steps=100
-[34, 35, 64, 10, 3, 13, 99, 17, 7, 14]
-Minutes: 255.10599056879678
+    +goal_pos=[3,-3]
+    +borders=[7,-7]
+    +max_steps=100
+    [34, 35, 64, 10, 3, 13, 99, 17, 7, 14]
+    Minutes: 255.10599056879678
 
 Params15:
-[63, 11, 11, 6, 38, 2, 99, 31, 99, 99]
-Minutes: 624.9840517878532
+    [63, 11, 11, 6, 38, 2, 99, 31, 99, 99]
+    Minutes: 624.9840517878532
 
 Params16:
-[2, 33, 11, 1, 22, 29, 1, 40, 99, 8]
-Minutes: 376.4243238766988
+    [2, 33, 11, 1, 22, 29, 1, 40, 99, 8]
+    Minutes: 376.4243238766988
 
 Params17:
-[4, 8, 7, 46, 3, 51, 62, 33, 1, 62]
-Minutes: 398.19128265380857
+    [4, 8, 7, 46, 3, 51, 62, 33, 1, 62]
+    Minutes: 398.19128265380857
 
 Params18:
-[7, 35, 13, 9, 5, 99, 99, 54, 99, 7]
-Minutes: 442.9775447527567
+    [7, 35, 13, 9, 5, 99, 99, 54, 99, 7]
+    Minutes: 442.9775447527567
 
 Params19:
-[18, 6, 29, 9, 99, 8, 5, 11, 18, 52]
-Minutes: 267.74789813756945
+    [18, 6, 29, 9, 99, 8, 5, 11, 18, 52]
+    Minutes: 267.74789813756945
 
 Params20:
-[32, 34, 99, 67, 1, 32, 99, 99, 2, 14]
-Minutes: 463.37765492598214
+    [32, 34, 99, 67, 1, 32, 99, 99, 2, 14]
+    Minutes: 463.37765492598214
 
 Params21:
-[3, 26, 99, 56, 2, 1, 4, 20, 18, 91]
-Minutes: 321.5921831091245
+    [3, 26, 99, 56, 2, 1, 4, 20, 18, 91]
+    Minutes: 321.5921831091245
 
 Params12_again:
-+multiprocessing numpy seed fix
-[37, 99, 9, 10, 13, 6, 11, 21, 7, 17]
-Minutes: 145.70517488718033
+    +multiprocessing numpy seed fix
+    [37, 99, 9, 10, 13, 6, 11, 21, 7, 17]
+    Minutes: 145.70517488718033
 
 Params19_again:
-+multiprocessing numpy seed fix
-[5, 17, 10, 2, 2, 3, 1, 9, 10, 2]
-Minutes: 57.76622135241826
+    +multiprocessing numpy seed fix
+    [5, 17, 10, 2, 2, 3, 1, 9, 10, 2]
+    Minutes: 57.76622135241826
 
 Params22:
-just testing Gridworld
+    just testing Gridworld
 
 Params23:
-just testing Gridworld
+    just testing Gridworld
 
 Params24:
-EVAL 1202212 0.7708295649609999
-Minutes: 76.87422110637029
-+dont quadruple rewards
-+do 10 runs (from now on charts)
-Running
+    EVAL 1202212 0.7708295649609999
+    Minutes: 76.87422110637029
+    +dont quadruple rewards
+    +do 10 runs (from now on charts)
+    Running
 
 Params25:
-EVAL 1202212 0.7708295649609999
-Minutes: 59.53636395931244
-Running
+    EVAL 1202212 0.7708295649609999
+    Minutes: 59.53636395931244
+    Running
 
 Params26:
-EVAL 1202212 0.7708295649609999
-Minutes: 120.59210259517035
-Running
+    EVAL 1202212 0.7708295649609999
+    Minutes: 120.59210259517035
+    Running
 
 Params27:
-EVAL 1202212 0.7708295649609999
-Minutes: 134.39488005638123
-Running
+    EVAL 1202212 0.7708295649609999
+    Minutes: 134.39488005638123
+    Running
 
 Params28:
-EVAL 122022 0.8008746470559999
-Minutes: 65.5352343996366
-Running
+    EVAL 122022 0.8008746470559999
+    Minutes: 65.5352343996366
+    Running
 
 """
 
